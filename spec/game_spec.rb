52ca2341ed5fd634390679f1dbd6b84524206954
Emmanuel Genard <edgenard@gmail.com>
2015-10-22 12:56:55 -0400

    Add tests to check that game is over when left and right diagonal is one marker, add .rspec to colorize output

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -117,5 +117,40 @@ RSpec.describe "Game" do
     expect(result).to be true
   end
 
+  it "is over if left diagonal is all O" do
+    game = Game.new
+    board = ["O", "1", "2", "3", "O", "5", "6" , "7", "O"]
+
+    result = game.game_is_over(board)
+
+    expect(result).to be true
+  end
+
+  it "is over if left diagonal is all X" do
+    game = Game.new
+    board = ["X", "1", "2", "3", "X", "5", "6" , "7", "X"]
+
+    result = game.game_is_over(board)
+
+    expect(result).to be true
+  end
+
+  it "is over if right diagonal is all O" do
+    game = Game.new
+    board = ["0", "1", "O", "3", "O", "5", "O" , "7", "8"]
+
+    result = game.game_is_over(board)
+
+    expect(result).to be true
+  end
+
+  it "is over if right diagonal is all X" do
+    game = Game.new
+    board = ["0", "1", "X", "3", "X", "5", "X" , "7", "8"]
+
+    result = game.game_is_over(board)
+
+    expect(result).to be true
+  end
 
 end
