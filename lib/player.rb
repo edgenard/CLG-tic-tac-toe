624151c2f3f6e0d05c20ad045b2ea745f9f459c6
Emmanuel Genard <edgenard@gmail.com>
2015-10-23 19:30:05 -0400

    Refactored get_best_move by extracting the iteration, player wins when it can, blocks the other player from winning

--- lib/player.rb
+++ lib/player.rb
@@ -9,4 +9,51 @@ class Player
   def human?
     @human
   end
+
+  def choose_spot(board)
+    return 4 if board[4] == '4'
+    get_best_move(board, mark)
+  end
+
+
+  private
+
+  def get_best_move(board, mark, depth = 0, best_score = {})
+    available_spaces = board.available_spaces
+    return available_spaces.first.to_i if available_spaces.length < 2
+
+    win = possible_move(available_spaces, board, mark)
+    if win
+      return win
+    else
+      block = possible_move(available_spaces, board, other_players_mark)
+      if block
+        return block
+      end
+    end
+
+
+    n = rand(0..available_spaces.count)
+    return available_spaces[n].to_i
+  end
+
+  def other_players_mark
+    mark == 'X' ? 'O' : 'X'
+  end
+
+  def possible_move(spaces, board, mark)
+
+    spaces.each do |space|
+      board[space.to_i] = mark
+      if board.game_over?
+        board[space.to_i] = space
+        return space.to_i
+      else
+        board[space.to_i] = space
+      end
+    end
+
+    return nil
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe "Player" do
 
   describe "Computer Player" do
     it "chooses the middle of the board if it is open" do
-      player = Player.new('X', true)
+      player = Player.new('X')
       board = Board.new
       board[1], board[5] = 'X', 'O'
 
@@ -37,5 +37,28 @@ RSpec.describe "Player" do
 
       expect(result).to eq(4)
     end
+
+    it "blocks other player from winning" do
+      player = Player.new('X')
+      board = Board.new
+      board[0], board[1], board[4] = 'O', 'O', 'X'
+
+      result = player.choose_spot(board)
+
+      expect(result).to eq(2)
+    end
+
+    it "wins when it can" do
+      player = Player.new('X')
+      board = Board.new
+      board[0], board[2], board[3] = 'O', 'O', 'O'
+      board[1], board[4] = 'X', 'X'
+
+      result = player.choose_spot(board)
+
+      expect(result).to eq(7) 
+    end
+
+
   end
 end
