6d583e6a92b0e2531ca93588bf328134766a8170
Emmanuel Genard <edgenard@gmail.com>
2015-10-23 14:08:59 -0400

    Setter and getter for board added

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,19 @@
 class Board
   attr_reader :board
   def initialize
-    @board = ["1", "2", "3", "4", "5", "6", "7", "8"]
+    @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
   end
+
+  def display
+    "|_#{board[0]}_|_#{board[1]}_|_#{board[2]}_|\n|_#{board[3]}_|_#{board[4]}_|_#{board[5]}_|\n|_#{board[6]}_|_#{board[7]}_|_#{board[8]}_|\n"
+  end
+
+  def [](idx)
+    @board[idx]
+  end
+
+  def []=(idx, value)
+    @board[idx] = value
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,14 +8,41 @@ RSpec.describe "Board" do
 
     result = board.board
 
-    expect(result).to match(["1", "2", "3", "4", "5", "6", "7", "8"])
+    expect(result).to match(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
   end
 
   it "displays the board" do
     board = Board.new
 
-    result = board.display_board
+    result = board.display
 
-    expect(result).to eq()
+    expect(result).to eq("|_0_|_1_|_2_|\n|_3_|_4_|_5_|\n|_6_|_7_|_8_|\n")
   end
+
+  it "returns the value of spot on the board" do
+    board = Board.new
+
+    result = board[0]
+
+    expect(result).to eq("0")
+  end
+
+  it "sets the value at the given index" do
+    board = Board.new
+
+    board[1] = "O"
+
+    expect(board[1]).to eq("O")
+  end
+
+  it "game is over if the top row is all O" do
+    board = Board.new
+    board[0], board[1], board[2] = "O", "O", "O"
+
+    result = board.game_over?
+
+    expect(result).to be true
+  end
+
+
 end
