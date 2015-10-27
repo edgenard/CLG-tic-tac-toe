6e8557e7bd30b7f4bb79710193db79387e61c582
Emmanuel Genard <edgenard@gmail.com>
2015-10-27 13:30:27 -0400

    refactored ui tests to have less repetition

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -30,43 +30,6 @@ RSpec.describe "Game" do
     expect(result).to be_kind_of(Repl)
   end
 
-  it "#start_game calls #play" do
-    game = Game.new
-    allow(game.repl).to receive(:print).and_return("")
-    allow(game).to receive(:play).and_return(nil)
-
-    game.start_game
-
-    expect(game).to have_received(:play)
-  end
-
-  it "plays until someone wins" do
-    game = Game.new
-    allow(game.repl).to receive(:print).and_return("")
-    allow(game).to receive(:get_human_spot).and_return(4)
-    allow(game.board).to receive(:game_over?).and_return(false, false,false, true)
-    allow(game.player2).to receive(:choose_spot).and_return(3)
-
-
-    game.play
-
-    expect(game).to have_received(:get_human_spot).once
-    expect(game.player2).to have_received(:choose_spot).once
-  end
-
-  it "stops playing if there is a tie" do
-    game = Game.new
-    allow(game.repl).to receive(:print).and_return("")
-    allow(game).to receive(:get_human_spot).and_return(4)
-    allow(game.board).to receive(:tie?).and_return(false, false,false, true)
-    allow(game.player2).to receive(:choose_spot).and_return(3)
-
-    game.play
-
-    expect(game).to have_received(:get_human_spot).once
-    expect(game.player2).to have_received(:choose_spot).once
-  end
-
   it "formats the board for the cli" do
     game = Game.new
 
@@ -75,30 +38,58 @@ RSpec.describe "Game" do
     expect(result).to eq("|_0_|_1_|_2_|\n|_3_|_4_|_5_|\n|_6_|_7_|_8_|\n")
   end
 
-  it "clears the board for after each turn" do
-    game = Game.new
-    allow(game.repl).to receive(:print).and_return("")
-    allow(game).to receive(:get_human_spot).and_return(4)
-    allow(game.board).to receive(:tie?).and_return(false, false,false, true)
-    allow(game.player2).to receive(:choose_spot).and_return(3)
-    allow(game.repl).to receive(:clear)
+  describe "game play" do
+    let(:game){ Game.new}
+    before(:each) do
 
-    game.play
+      allow(game.repl).to receive(:print).and_return("")
+      allow(game).to receive(:get_human_spot).and_return(4)
+      allow(game.player2).to receive(:choose_spot).and_return(3)
+    end
 
-    expect(game.repl).to have_received(:clear).once
-  end
+    it "#start_game calls #play" do
+      allow(game).to receive(:play).and_return(nil)
 
-  it "notifies the user of computer choice" do
-    game = Game.new
-    allow(game.repl).to receive(:print).and_return("")
-    allow(game).to receive(:get_human_spot).and_return(4)
-    allow(game.board).to receive(:tie?).and_return(false, false,false, true)
-    allow(game.player2).to receive(:choose_spot).and_return(3)
-    allow(game.repl).to receive(:clear)
+      game.start_game
 
-    game.play
+      expect(game).to have_received(:play)
+    end
 
-    expect(game.repl).to have_received(:print).with("Player 2 chose 3")
+    it "plays until someone wins" do
+      allow(game.board).to receive(:game_over?).and_return(false, false,false, true)
+
+      game.play
+
+      expect(game).to have_received(:get_human_spot).once
+      expect(game.player2).to have_received(:choose_spot).once
+    end
+
+    it "stops playing if there is a tie" do
+      allow(game.board).to receive(:tie?).and_return(false, false,false, true)
+
+      game.play
+
+      expect(game).to have_received(:get_human_spot).once
+      expect(game.player2).to have_received(:choose_spot).once
+    end
+
+
+    it "clears the board for after each turn" do
+      allow(game.board).to receive(:game_over?).and_return(false, false,false, true)
+      allow(game.repl).to receive(:clear)
+
+      game.play
+
+      expect(game.repl).to have_received(:clear).once
+    end
+
+    it "notifies the user of computer choice" do
+      allow(game.board).to receive(:game_over?).and_return(false, false,false, true)
+      game.play
+
+      expect(game.repl).to have_received(:print).with("Player 2 chose 3")
+    end
+
   end
 
 
