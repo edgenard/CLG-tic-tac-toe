7396311e2b1fbb1cedcb980128eb52f3fdb1b7ce
Emmanuel Genard <edgenard@gmail.com>
2015-11-05 18:40:29 -0500

    Add tests for 4x4 board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe "Board" do
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if top row is all X" do
@@ -55,7 +55,7 @@ RSpec.describe "Board" do
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if middle row is all O" do
@@ -63,7 +63,7 @@ RSpec.describe "Board" do
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if middle row is all X" do
@@ -71,7 +71,7 @@ RSpec.describe "Board" do
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if bottom row is all O" do
@@ -79,7 +79,7 @@ RSpec.describe "Board" do
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if bottom row is all X" do
@@ -87,121 +87,480 @@ RSpec.describe "Board" do
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the first column is all O" do
-      board[0], board[3], board[6] = 'O', 'O', 'O'
+      board[0], board[3], board[6] = "O", "O", "O"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the first column is all X" do
-      board[0], board[3], board[6] = 'X', 'X', 'X'
+      board[0], board[3], board[6] = "X", "X", "X"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the middle column is all O" do
-      board[1], board[4], board[7] = 'O', 'O', 'O'
+      board[1], board[4], board[7] = "O", "O", "O"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the middle column is all X" do
-      board[1], board[4], board[7] = 'X', 'X', 'X'
+      board[1], board[4], board[7] = "X", "X", "X"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the last column is all O" do
-      board[2], board[5], board[8] = 'O', 'O', 'O'
+      board[2], board[5], board[8] = "O", "O", "O"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the last column is all X" do
-      board[2], board[5], board[8] = 'X', 'X', 'X'
+      board[2], board[5], board[8] = "X", "X", "X"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the left diagonal is all O" do
-      board[0], board[4], board[8] = 'O', 'O', 'O'
+      board[0], board[4], board[8] = "O", "O", "O"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the left diagonal is all X" do
-      board[0], board[4], board[8] = 'X', 'X', 'X'
+      board[0], board[4], board[8] = "X", "X", "X"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the right diagonal is all O" do
-      board[2], board[4], board[6] = 'O', 'O', 'O'
+      board[2], board[4], board[6] = "O", "O", "O"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "game is over if the right diagonal is all X" do
-      board[2], board[4], board[6] = 'X', 'X', 'X'
+      board[2], board[4], board[6] = "X", "X", "X"
 
       result = board.game_over?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "is a tie if the all places are filled without a winner" do
-      board[0], board[1], board[2] = 'X', 'O', 'X'
-      board[3], board[4], board[5] = 'O', 'O', 'X'
-      board[6], board[7], board[8] = 'O', 'X', 'O'
+      board[0], board[1], board[2] = "X", "O", "X"
+      board[3], board[4], board[5] = "O", "O", "X"
+      board[6], board[7], board[8] = "O", "X", "O"
 
       result = board.tie?
 
-      expect(result).to be true
+      expect(result).to be_truthy
     end
 
     it "is not a tie when X has won" do
-      board[0], board[4], board[8] = 'X', 'X', 'X'
+      board[0], board[4], board[8] = "X", "X", "X"
 
       result = board.tie?
 
-      expect(result).to be false
+      expect(result).to be_falsy
     end
 
     it "is not a tie when O has won" do
-      board[6], board[7], board[8] = 'O', 'O', 'O'
+      board[6], board[7], board[8] = "O", "O", "O"
 
       result = board.tie?
 
-      expect(result).to be false
+      expect(result).to be_falsy
     end
 
     it "returns the available spaces" do
-      board[1], board[3], board[6] = 'X', 'O', 'X'
+      board[1], board[3], board[6] = "X", "O", "X"
 
       result = board.available_spaces
 
       expect(result).to match(['0','2','4', '5', '7', '8'])
+    end
+  end
+
+  describe "4x4 board " do
+    let (:board) { Board.new(4)}
+
+    it "game is over if the first row is all O" do
+      board[0], board[1], board[2], board[3] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if only some of the first row is O" do
+      board[0], board[1], board[2] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if first row is all X" do
+      board[0], board[1], board[2], board[3] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if only some of the first is X" do
+      board[0], board[1], board[2] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if second row is all O" do
+      board[4], board[5], board[6], board[7] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of the second row is O" do
+      board[4], board[5], board[6] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if second row is all X" do
+      board[4], board[5], board[6], board[7] = "X", "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of the second row is X" do
+      board[4], board[5], board[6] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if third row is all O" do
+      board[8], board[9], board[10], board[11] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of third row is O" do
+      board[8], board[9], board[10] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if third row is all X" do
+      board[8], board[9], board[10], board[11] = "X", "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of third row is X" do
+      board[8], board[9], board[10] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if fourth row is all O" do
+      board[12], board[13], board[14], board[15] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of fourth row is O" do
+      board[12], board[13], board[14] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if all of fourth row is X" do
+      board[12], board[13], board[14], board[15] = "X", "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of fourth row is X" do
+      board[12], board[13], board[14] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the first column is all O" do
+      board[0], board[4], board[8], board[12] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of first colum is O" do
+      board[0], board[4], board[8] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the first column is all X" do
+      board[0], board[4], board[8], board[12] = "X", "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of first column is X" do
+      board[0], board[4], board[8] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the second column is all O" do
+      board[1], board[5], board[9], board[13] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of second column is O" do
+      board[1], board[5], board[9] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the second column is all X" do
+      board[1], board[5], board[9], board[13] = "X", "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of second column is X" do
+      board[1], board[5], board[9] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the third column is all O" do
+      board[2], board[6], board[10], board[14] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of third O" do
+      board[2], board[6], board[10] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the third column is all X" do
+      board[2], board[6], board[10], board[14] = "X", "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of third column is X" do
+      board[2], board[6], board[10] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the fourth column is all O" do
+      board[3], board[7], board[11], board[15] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of fourth column is O" do
+      board[3], board[7], board[11] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the fourth column is all X" do
+      board[3], board[7], board[11], board[15] = "X", "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of fourth column is X" do
+      board[3], board[7], board[11] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the right diagonal is all O" do
+      board[3], board[6], board[9], board[12] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not  over if some of right diagonal is O" do
+      board[3], board[6], board[9] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the right diagonal is all X" do
+      board[3], board[6], board[9], board[12] = "X", "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not  over if some of right diagonal is X" do
+      board[3], board[6], board[9] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the left diagonal is all O" do
+      board[0], board[5], board[10], board[15] = "O", "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of the left diagonal is O" do
+      board[0], board[5], board[10] = "O", "O", "O"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is over if the left diagonal is all X" do
+      board[0], board[5], board[10], board[15] = "X", "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_truthy
+    end
+
+    it "game is not over if some of the left diagonal is X" do
+      board[0], board[5], board[10] = "X", "X", "X"
+
+      result = board.game_over?
+
+      expect(result).to be_falsy
+    end
+
+    it "game is a tie if the all places are filled without a winner" do
+      board[0], board[1], board[2], board[3]     = "X", "O", "X", "O"
+      board[4], board[5], board[6], board[7]     = "O", "O", "X", "X"
+      board[8], board[9], board[10], board[11]   = "O", "X", "O", "O"
+      board[12], board[13], board[14], board[15] = "O", "X", "X", "O"
+
+      result = board.tie?
+
+      expect(result).to be_truthy
+    end
+
+    it "is not a tie when X has won" do
+      board[0], board[4], board[8], board[12] = "X", "X", "X", "X"
+
+      result = board.tie?
+
+      expect(result).to be_falsy
+    end
+
+    it "is not a tie when O has won" do
+      board[3], board[6], board[9], board[12] = "O", "O", "O", "O"
+
+      result = board.tie?
+
+      expect(result).to be_falsy
+    end
+
+    it "returns the available spaces" do
+      board[1], board[3], board[6], board[10] = "X", "O", "X", "O"
+
+      result = board.available_spaces
+
+      expect(result).to match(%w[0 2 4 5 7 8 9 11 12 13 14 15 ])
     end
   end
 end
